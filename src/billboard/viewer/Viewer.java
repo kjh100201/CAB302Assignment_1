6a2223316fdeea56bcfe48ac42fc17bc5fa47224
HanssenJarrah <55371005+HanssenJarrah@users.noreply.github.com>
2020-04-28 12:32:50 +1000

    Updated source root

--- src/billboard/viewer/Viewer.java
+++ src/billboard/viewer/Viewer.java
@@ -9,7 +9,7 @@ public class Viewer {
     public static void main(String[] args) {
         // Create the billboard viewer GUI object
         BillboardFrame billboard = new BillboardFrame();
-
+        
         billboard.setVisible(true);
     }
 }
